0a9e0ff25df01540a3110ad5893dfe43d14ed49a
chenwei.sun <chenwei.sun@intel.com>
2025-02-25 10:56:06 +0800

    add transpose example
    Signed-off-by: chenwei.sun <chenwei.sun@intel.com>

--- ldmatrix/main_8by8.cu
+++ ldmatrix/main_8by8.cu
@@ -8,7 +8,7 @@ __global__ void test_ldmatrix(half *load, half *store) {
   const int start_index = threadIdx.x %8 * 8;
   *(float4 *)(&(smem[start_index])) = *(float4 *)(&(load[start_index]));
 
-  asm("ldmatrix.sync.aligned.m8n8.x1.b16 {%0}, [%1];\n\t"
+  asm("ldmatrix.sync.aligned.m8n8.x1.trans.b16 {%0}, [%1];\n\t"
       : "=r"(reg[0]): "l"(&smem[start_index]));
 
   *(float *)(&(store[threadIdx.x * 2])) = *(float *)(&(reg[0]));
@@ -29,11 +29,37 @@ void init_mem(half *ptr, int N) {
 }
 
 int main() {
-  half h_in[8 * 8];
+  //half h_in[8 * 8];
+  //init_mem(h_in, 8 * 8);
+    
   half h_out[8 * 8];
   half *d_in, *d_out;
 
-  init_mem(h_in, 8 * 8);
+
+
+  half h_in[64] = {
+      0, 1, 2, 3, 4, 5, 6, 7,
+      8, 9, 10,11,12,13,14,15,
+      16,17,18,19,20,21,22,23,
+      24,25,26,27,28,29,30,31,
+      32,33,34,35,36,37,38,39,
+      40,41,42,43,44,45,46,47,
+      48,49,50,51,52,53,54,55,
+      56,57,58,59,60,61,62,63
+  };
+#if 1
+  half h_in_trans[64] = {
+    0,8, 16,24,32,40,48,56,
+    1, 9, 17,25,33,41,49,57,
+    2, 10,18,26,34,42,50,58,
+    3, 11,19,27,35,43,51,59,
+    4, 12,20,28,36,44,52,60,
+    5, 13,21,29,37,45,53,61,
+    6, 14,22,30,38,46,54,62,
+    7,15,23,31,39,47,55,63
+  };
+#endif
+
 
   constexpr int data_size = 8 * 8 * sizeof(half);
 
@@ -51,6 +77,5 @@ int main() {
     printf("thread %d  holds: ", i);
     printf("%f %f \n", __half2float(h_out[i*2]), __half2float(h_out[i*2 + 1]));
   }
-
   return 0;
 }
